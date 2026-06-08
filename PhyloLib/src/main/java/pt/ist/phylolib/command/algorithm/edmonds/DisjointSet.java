887bbfddcf853633c9cb353d20cd5560c99ef087
Goncalo Palma Nunes <goncalopalmanunes@tecnico.ulisboa.pt>
2026-06-08 11:33:59 +0100

    change class and method visibility for DisjointSets

--- PhyloLib/src/main/java/pt/ist/phylolib/command/algorithm/edmonds/DisjointSet.java
+++ PhyloLib/src/main/java/pt/ist/phylolib/command/algorithm/edmonds/DisjointSet.java
@@ -1,12 +1,12 @@
 package pt.ist.phylolib.command.algorithm.edmonds;
 
-class DisjointSet {
+public class DisjointSet {
 
 	protected final int size;
 	protected final int[] pi;
 	protected final int[] rank;
 
-	DisjointSet(int n) {
+	public DisjointSet(int n) {
 		this.size = n + 1;
 		this.pi = new int[size];
 		this.rank = new int[size];
@@ -14,17 +14,17 @@ class DisjointSet {
 			this.pi[i] = i;
 	}
 
-	int findSet(int i) {
+	public int findSet(int i) {
 		for (; i != pi[i]; i = pi[i])
 			pi[i] = pi[pi[i]];
 		return i;
 	}
 
-	boolean sameSet(int i, int j) {
+	public boolean sameSet(int i, int j) {
 		return findSet(i) == findSet(j);
 	}
 
-	void unionSet(int i, int j) {
+	public void unionSet(int i, int j) {
 		i = findSet(i);
 		j = findSet(j);
 		if (i != j) {
