d2ba1d31aeb65fba1dd6c18073ab0775dbb464b0
Goncalo Palma Nunes <goncalopalmanunes@tecnico.ulisboa.pt>
2026-07-23 16:08:59 +0100

    fix: grapetree directional distance correction

--- PhyloLib/src/main/java/pt/ist/phylolib/command/distance/GrapeTree.java
+++ PhyloLib/src/main/java/pt/ist/phylolib/command/distance/GrapeTree.java
@@ -21,7 +21,7 @@ public final class GrapeTree extends Distance {
 		for (int l = 0; l < i.size(); l++) {
 			if (j.locus(l) != null) {
 				nonmissing++;
-				if (!j.locus(l).equals(i.locus(l)))
+				if (i.locus(l) == null || !i.locus(l).equals(j.locus(l)))
 					differences++;
 			}
 		}
