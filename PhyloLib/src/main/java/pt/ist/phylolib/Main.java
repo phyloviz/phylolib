5265474aa68418af20a46a766760e00c4bcabbce
Gonçalo Nunes <73646319+Goncalo-Palma-Nunes@users.noreply.github.com>
2026-09-07 14:05:40 +0100

    formatting: spacing issue

--- PhyloLib/src/main/java/pt/ist/phylolib/Main.java
+++ PhyloLib/src/main/java/pt/ist/phylolib/Main.java
@@ -24,7 +24,7 @@ public class Main {
 				Context context = new Context();
 				ICommand.run(arguments, context, Command.DISTANCE, context::getDataset, context::setMatrix);
 				ICommand.run(arguments, context, Command.CORRECTION, context::getMatrix, context::setMatrix);
-			ICommand.run(arguments, context, Command.ALGORITHM, context::getMatrix, context::setTree);
+				ICommand.run(arguments, context, Command.ALGORITHM, context::getMatrix, context::setTree);
 				ICommand.run(arguments, context, Command.OPTIMIZATION, context::getTree, context::setTree);
 
 				long programEnd = System.nanoTime();
